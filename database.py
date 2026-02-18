96ffece6088a02e5be1d78696eb1cd250b980a99
Daniil Bojarczuk <boyarchuk2007@gmail.com>
2026-02-18 16:25:06 +0100

    CRUD for users and transactions

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/expense_db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
